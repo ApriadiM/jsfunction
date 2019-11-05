469f88406a1b4e089647b0069390416c770dfa43
ApriadiM <apriadi527@yahoo.com>
2019-11-05 16:16:40 +0700

    function const check age

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Function Backtik
 function logName(name) {
   console.log(`Hello, ${name}!`);
 }
@@ -7,3 +8,18 @@ const nameB = "Maya";
 
 logName(nameA);
 logName(nameB);
+
+//Function const
+const checkAge = function(age) {
+  if (age >= 18) {
+    console.log("You are old enough");
+  } else if (age < 18 && age >= 0) {
+    console.log("You are still young");
+  } else {
+    console.log("You are not born yet");
+  }
+};
+
+checkAge(10); // You are still young
+checkAge(25); // You are old enough
+checkAge(-2); // You are not born yet
